46d417866d4f3a85a13da80e0e0465d08b3227a3
Hexy32 <sunshine.goody8762@gmail.com>
2023-01-06 13:29:28 -0500

    Little fixes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ import scrollTo from '../scripts/scrollTo'
 function Home() {
   return (
     <div className={s.heroSection}>
-      <h1 className={s.hero}>Font-End Web Developer</h1>
+      <h1 className={s.hero}>Front-End Web Developer</h1>
       <p className={s.subtitle}>React &#x2022; TypeScript &#x2022; Sass</p>
       <button onClick={() => scrollTo('Projects')} className={s.scrollIndicator}>
         <span className=" material-symbols-rounded">south</span>
